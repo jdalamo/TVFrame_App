3656762c1d998ecbea0a84df0fefeb920f6f4ae4
jdalamo <alamojad@gmail.com>
2020-07-29 19:05:10 -0500

    put request now made after successfully scanning qr code to update connected status on server

--- TVFrame/ScannerViewController.swift
+++ TVFrame/ScannerViewController.swift
@@ -109,5 +109,63 @@ extension ScannerViewController : AVCaptureMetadataOutputObjectsDelegate {
     
     func found(ip: String) {
         UserDefaults.standard.set(ip, forKey: "ServerIpAddress")
+        updateConnectionStatus()
+    }
+    
+    func updateConnectionStatus() {
+        let url_end = "connection_status/"
+        let conn = APIConnection()
+        let url = conn.baseURL.appendingPathComponent(url_end)
+        
+        struct UploadData: Codable {
+            let data: Bool
+        }
+        
+        let uploadDataModel = UploadData(data: true)
+        
+        guard let jsonData = try? JSONEncoder().encode(uploadDataModel) else {
+            print("error encoding")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print("error calling put")
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("did not receive data")
+                return
+            }
+            guard let response = response as? HTTPURLResponse, (200..<299) ~= response.statusCode else {
+                print("http request failed")
+                return
+            }
+            do {
+                guard let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+                    print("cannot convert data to JSON object")
+                    return
+                }
+                guard let prettyJsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted) else {
+                    print("cannot convert JSON object to pretty json data")
+                    return
+                }
+                guard let prettyPrintedJson = String(data: prettyJsonData, encoding: .utf8) else {
+                    print("couldn't print json in string")
+                    return
+                }
+                
+                print(prettyPrintedJson)
+            } catch {
+                print("error trying to convert json data to string")
+                return
+            }
+            
+        }.resume()
     }
 }
